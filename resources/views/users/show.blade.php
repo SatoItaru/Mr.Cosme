15097c901a7c1499dacd5ac37d87833ad7e4a617
SatoItaru <itaru1822@gmail.com>
2021-12-24 16:52:10 +0900

    col edit

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -48,7 +48,8 @@
                     </nav>
                     <div class="tab-content" id="nav-tabContent">
                         <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                            <div class="row">
+                            <div class="container">
+                            <div class="row justify-content-center">
                                 @if($posts -> isEmpty())
                                 <div class="card col-md-12 my-3 mx-auto">
                                     <p class="text-center my-3 text-muted">まだ投稿がありません。<br>あなたのお気に入りのコスメを投稿してみましょう。</p>
@@ -106,69 +107,73 @@
                                     @endif
                                 </div>
                             </div>
+                        </div>
                             <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                                    <div class="row">
+                                <div class="container">
+                                    <div class="row justify-content-center">
                                         @if($posts -> isEmpty())
-                                    <div class="card col-md-12 my-3 mx-auto">
-                                        <p class="text-center my-3 text-muted">まだ投稿がありません。<br>あなたのお気に入りのBBクリームを投稿してみましょう。</p>
-                                        <div class="text-center">
-                                            <a href="{{ route('posts.create') }}" class="col-md-3 detail btn-anime bgleft mx-auto text-muted my-3"><span><i class="fas fa-plus"></i>投稿する</span></a>
+                                        <div class="card col-md-12 my-3 mx-auto">
+                                            <p class="text-center my-3 text-muted">まだ投稿がありません。<br>あなたのお気に入りのBBクリームを投稿してみましょう。</p>
+                                            <div class="text-center">
+                                                <a href="{{ route('posts.create') }}" class="col-md-3 detail btn-anime bgleft mx-auto text-muted my-3"><span><i class="fas fa-plus"></i>投稿する</span></a>
+                                            </div>
+                                            <div class="col-md-12 text-center py-3">
+                                                <img src="https://res.cloudinary.com/dqxuxpwv9/image/upload/v1640061684/BB%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%A0_fmnj9g.jpg" alt="画像" class="rounded-circle img-thumbnail mb-3">
+                                                <img src="https://res.cloudinary.com/dqxuxpwv9/image/upload/v1640062165/%E3%82%B3%E3%83%B3%E3%82%B7%E3%83%BC%E3%83%A9%E3%83%BC_yjm46r.jpg" alt="画像" class="rounded-circle img-thumbnail mb-3">
+                                                <img src="https://res.cloudinary.com/dqxuxpwv9/image/upload/v1640062004/%E3%83%95%E3%82%A1%E3%83%B3%E3%83%86%E3%82%99%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3_l7smmq.jpg" alt="画像" class="rounded-circle img-thumbnail mb-3">
+                                            </div>
                                         </div>
-                                        <div class="col-md-12 text-center py-3">
-                                            <img src="https://res.cloudinary.com/dqxuxpwv9/image/upload/v1640061684/BB%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%A0_fmnj9g.jpg" alt="画像" class="rounded-circle img-thumbnail mb-3">
-                                            <img src="https://res.cloudinary.com/dqxuxpwv9/image/upload/v1640062165/%E3%82%B3%E3%83%B3%E3%82%B7%E3%83%BC%E3%83%A9%E3%83%BC_yjm46r.jpg" alt="画像" class="rounded-circle img-thumbnail mb-3">
-                                            <img src="https://res.cloudinary.com/dqxuxpwv9/image/upload/v1640062004/%E3%83%95%E3%82%A1%E3%83%B3%E3%83%86%E3%82%99%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3_l7smmq.jpg" alt="画像" class="rounded-circle img-thumbnail mb-3">
-                                        </div>
-                                    </div>
-                                    @else
-                                    @foreach ($posts as $post)
-                                    @if ($post->cosme === 'BBクリーム')
-                                    <div class="col-md-4 my-2">
-                                        <div class="card" style="width: 17rem;">
-                                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                                <ol class="carousel-indicators">
-                                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                                                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                                                </ol>
-                                                <div class="carousel-inner">
-                                                    @foreach ($post->images as $image)
-                                                    <div class="carousel-item @if($loop->first) active @endif">
-                                                        <img class="image card-img-top d-block w-20" src="{{ $image->image_path }}" alt="Card image cap　slide image">
+                                        @else
+                                        @foreach ($posts as $post)
+                                        @if ($post->cosme === 'BBクリーム')
+                                        <div class="col-md-4 my-2">
+                                            <div class="card" style="width: 17rem;">
+                                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                                    <ol class="carousel-indicators">
+                                                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                                                    </ol>
+                                                    <div class="carousel-inner">
+                                                        @foreach ($post->images as $image)
+                                                        <div class="carousel-item @if($loop->first) active @endif">
+                                                            <img class="image card-img-top d-block w-20" src="{{ $image->image_path }}" alt="Card image cap　slide image">
+                                                        </div>
+                                                        @endforeach
                                                     </div>
-                                                    @endforeach
+
+                                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                        <span class="sr-only">Previous</span>
+                                                    </a>
+
+                                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                        <span class="sr-only">Next</span>
+                                                    </a>
+
                                                 </div>
-
-                                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                                    <span class="sr-only">Previous</span>
-                                                </a>
-
-                                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                                    <span class="sr-only">Next</span>
-                                                </a>
-
-                                            </div>
-                                            <div class="card-body">
-                                                <h2 class="text-center text-muted">{{ $post->brand }}</h2>
-                                                <p class="text-center text-muted">{{ $post->cosme }}</p>
-                                                <p class="text-center text-muted">{{ $post->price }}円</p>
-                                                <a href="{{ route('posts.show', $post->id) }}" class="detail btn-anime bgleft"><span>詳細へ</span></a>
-                                            </div>
-                                            <div class="card-footer created">
-                                                <p class="text-center text-muted">投稿日:{{ $post->created_at }}</p>
+                                                <div class="card-body">
+                                                    <h2 class="text-center text-muted">{{ $post->brand }}</h2>
+                                                    <p class="text-center text-muted">{{ $post->cosme }}</p>
+                                                    <p class="text-center text-muted">{{ $post->price }}円</p>
+                                                    <a href="{{ route('posts.show', $post->id) }}" class="detail btn-anime bgleft"><span>詳細へ</span></a>
+                                                </div>
+                                                <div class="card-footer created">
+                                                    <p class="text-center text-muted">投稿日:{{ $post->created_at }}</p>
+                                                </div>
                                             </div>
                                         </div>
+                                            @endif
+                                        @endforeach
+                                        @endif
                                     </div>
-                                    @endif
-                                    @endforeach
-                                    @endif
                                 </div>
-                            </div>
+                                </div>
                             <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                                <div class="row">
+                                <div class="container">
+                                <div class="row justify-content-center">
                                     @if($posts -> isEmpty())
                                     <div class="card col-md-12 my-3 mx-auto">
                                         <p class="text-center my-3 text-muted">まだ投稿がありません。<br>あなたのお気に入りのコンシーラーを投稿してみましょう。</p>
@@ -227,9 +232,11 @@
                                     @endforeach
                                     @endif
                                 </div>
+                                </div>
                             </div>
                             <div class="tab-pane fade" id="nav-item" role="tabpanel" aria-labelledby="nav-item-tab">
-                                <div class="row">
+                                <div class="container">
+                                    <div class="row justify-content-center">
                                     @if($posts -> isEmpty())
                                     <div class="card col-md-12 my-3 mx-auto">
                                         <p class="text-center my-3 text-muted">まだ投稿がありません。<br>あなたのお気に入りのフェイスパウダーを投稿してみましょう。</p>
@@ -292,6 +299,7 @@
                             </div>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
     </div>
